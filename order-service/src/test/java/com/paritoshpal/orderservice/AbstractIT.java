4594a4866db244e954a7c21fa2d75dab3ef51a8c
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-01-26 21:32:30 +0000

    Tests now run in randomized ports

--- order-service/src/test/java/com/paritoshpal/orderservice/AbstractIT.java
+++ order-service/src/test/java/com/paritoshpal/orderservice/AbstractIT.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIT {
 
     @LocalServerPort
--- order-service/src/test/java/com/paritoshpal/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/paritoshpal/orderservice/OrderServiceApplicationTests.java
@@ -1,8 +1,6 @@
 package com.paritoshpal.orderservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 
 class OrderServiceApplicationTests  extends AbstractIT{
