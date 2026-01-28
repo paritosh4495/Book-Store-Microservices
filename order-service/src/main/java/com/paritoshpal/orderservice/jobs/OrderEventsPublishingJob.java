e9aaeb7e93c276398f163c360619c11726c35f59
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-01-28 18:35:08 +0000

    Added shedlock

--- order-service/src/main/java/com/paritoshpal/orderservice/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/paritoshpal/orderservice/jobs/OrderEventsPublishingJob.java
@@ -2,6 +2,8 @@ package com.paritoshpal.orderservice.jobs;
 
 import com.paritoshpal.orderservice.domain.OrderEventService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,7 +20,9 @@ public class OrderEventsPublishingJob {
     private final OrderEventService orderEventService;
 
     @Scheduled(cron = "${orders.publish-order-events-job-cron}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishOrderEvents(){
+        LockAssert.assertLocked();
         log.info("Publishing order events job started at {}", Instant.now());
         // Implementation for publishing order events goes here
         orderEventService.publishOrderEvents();
--- order-service/src/main/java/com/paritoshpal/orderservice/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/com/paritoshpal/orderservice/jobs/OrderProcessingJob.java
@@ -2,6 +2,8 @@ package com.paritoshpal.orderservice.jobs;
 
 import com.paritoshpal.orderservice.domain.OrderService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,7 +18,9 @@ public class OrderProcessingJob {
    private static final Logger log = LoggerFactory.getLogger(OrderProcessingJob.class);
 
    @Scheduled(cron = "${orders.new-orders-job-cron}")
+   @SchedulerLock(name = "processNewOrders")
    public void processNewOrders(){
+       LockAssert.assertLocked();
          log.info("Order processing job started");
          orderService.processNewOrders();
 
