dfb009830b3128a7380a0efb057cce33360e1b40
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-01-28 08:12:58 +0000

    Added TimeOut to the call to Product Controller end point from OrderController

--- order-service/src/main/java/com/paritoshpal/orderservice/clients/catalog/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/paritoshpal/orderservice/clients/catalog/CatalogServiceClientConfig.java
@@ -1,18 +1,31 @@
 package com.paritoshpal.orderservice.clients.catalog;
 
 import com.paritoshpal.orderservice.ApplicationProperties;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class CatalogServiceClientConfig {
 
     // Configure Rest Client as a bean
     @Bean
-    RestClient restClient(ApplicationProperties properties) {
-        return RestClient.builder()
+    RestClient restClient(RestClient.Builder builder, ApplicationProperties properties) {
+
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
+                .withCustomizer(customizer -> {
+                    customizer.setConnectTimeout(Duration.ofSeconds(5));
+                    customizer.setReadTimeout(Duration.ofSeconds(5));
+                })
+                .build();
+
+        return builder
                 .baseUrl(properties.catalogServiceUrl())
+                .requestFactory(requestFactory)
                 .build();
     }
 }
