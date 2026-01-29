ef52782d2cb9c297a6fdba6d5526054763fc342d
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-01-29 14:08:10 +0000

    Notification Event Handler now checks if the event is duplicate

---
+++ notification-service/src/main/java/com/paritoshpal/notificationservice/domain/OrderEventEntity.java
@@ -0,0 +1,33 @@
+package com.paritoshpal.notificationservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderEventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_event_id_generator")
+    @SequenceGenerator(name = "order_event_id_generator", sequenceName = "order_event_id_seq")
+    private Long id;
+
+    @Column(name = "event_id", nullable = false, unique = true)
+    private String eventId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
--- notification-service/src/main/java/com/paritoshpal/notificationservice/events/OrderEventHandler.java
+++ notification-service/src/main/java/com/paritoshpal/notificationservice/events/OrderEventHandler.java
@@ -1,11 +1,14 @@
 package com.paritoshpal.notificationservice.events;
 
 import com.paritoshpal.notificationservice.domain.NotificationService;
+import com.paritoshpal.notificationservice.domain.OrderEventRepository;
 import com.paritoshpal.notificationservice.domain.models.OrderCancelledEvent;
 import com.paritoshpal.notificationservice.domain.models.OrderCreatedEvent;
 import com.paritoshpal.notificationservice.domain.models.OrderDeliveredEvent;
 import com.paritoshpal.notificationservice.domain.models.OrderErrorEvent;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -13,33 +16,52 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 class OrderEventHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderEventHandler.class);
+
     private final NotificationService notificationService;
+    private final OrderEventRepository orderEventRepository;
 
     @RabbitListener(queues = "${notifications.new-orders-queue}")
     void handleOrderCreatedEvent(OrderCreatedEvent event) {
         // Handle the order created event (e.g., send notification)
-        System.out.println("Handling Order Created Event for Order ID: " + event);
+        log.info("Handling Order Created Event for Order ID: {}", event);
+        if(orderEventRepository.existsByEventId(event.eventId())){
+            log.warn("Duplicate Order Created Event received with Event ID: {}", event.eventId());
+            return;
+        }
         notificationService.sendOrderCreatedNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.delivered-orders-queue}")
     void handleOrderDeliveredEvent(OrderDeliveredEvent event) {
         // Handle the order delivered event (e.g., send notification)
-        System.out.println("Handling Order Delivered Event: " + event);
+        log.info("Handling Order Delivered Event: {}", event);
+        if(orderEventRepository.existsByEventId(event.eventId())){
+            log.warn("Duplicate Order Delivered Event received with Event ID: {}", event.eventId());
+            return;
+        }
         notificationService.sendOrderDeliveredNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.cancelled-orders-queue}")
     void handleOrderCancelledEvent(OrderCancelledEvent event) {
         // Handle the order cancelled event (e.g., send notification)
-        System.out.println("Handling Order Cancelled Event: " + event);
+        log.info("Handling Order Cancelled Event: {}", event);
+        if(orderEventRepository.existsByEventId(event.eventId())){
+            log.warn("Duplicate Order Cancelled Event received with Event ID: {}", event.eventId());
+            return;
+        }
         notificationService.sendOrderCancelledNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.error-orders-queue}")
     void handleOrderErrorEvent(OrderErrorEvent event) {
         // Handle the order error event (e.g., send notification)
-        System.out.println("Handling Order Error Event: " + event);
+        log.info("Handling Order Error Event: {}", event);
+        if(orderEventRepository.existsByEventId(event.eventId())){
+            log.warn("Duplicate Order Error Event received with Event ID: {}", event.eventId());
+            return;
+        }
         notificationService.sendOrderErrorEventNotification(event);
     }
 
