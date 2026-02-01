a6df780e19727713f32332e23fbef96e0643085f
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-02-01 18:07:34 +0000

    Spring Security Implementation - OAuth2

---
+++ bookstore-webapp/src/main/java/com/paritoshpal/bookstorewebapp/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.paritoshpal.bookstorewebapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(  "/js/*",
+                                "/css/*",
+                                "/images/*",
+                                "/error",
+                                "/webjars/**",
+                                "/",
+                                "/actuator/**",
+                                "/products/**",
+                                "/api/products/**"
+                        ).permitAll()
+                                .anyRequest().authenticated())
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.clearAuthentication(true)
+                .invalidateHttpSession(true)
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler()));
+        return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
+    }
+}
