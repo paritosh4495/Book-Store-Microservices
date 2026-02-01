db4752eb86f6fdf1ece77457058ec5d796b02815
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-02-01 18:32:11 +0000

    Order Service Security

--- order-service/src/main/java/com/paritoshpal/orderservice/domain/SecurityService.java
+++ order-service/src/main/java/com/paritoshpal/orderservice/domain/SecurityService.java
@@ -1,12 +1,21 @@
 package com.paritoshpal.orderservice.domain;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityService {
 
     public String getLoginUserName(){
-        return "user";
+
+        JwtAuthenticationToken authentication = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+
+        return jwt.getClaimAsString("preferred_username");
+
     }
 
 }
